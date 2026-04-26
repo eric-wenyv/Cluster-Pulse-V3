35ff4be6a2ea7c21dbde61e4a7ad466c35af5c05
eric <15935985812lhx@gmail.com>
2026-04-26 17:53:12 +0800

    feat: add tooltip composable

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { provide, ref } from 'vue';
+import { TooltipKey, type TooltipApi } from '../composables/useTooltip';
+
+const visible = ref(false);
+const html = ref('');
+const x = ref(0);
+const y = ref(0);
+
+const api: TooltipApi = {
+  show: (nextX, nextY, nextHtml) => {
+    html.value = nextHtml;
+    x.value = nextX;
+    y.value = nextY;
+    visible.value = true;
+  },
+  hide: () => {
+    visible.value = false;
+  }
+};
+
+provide(TooltipKey, api);
+</script>
+
+<template>
+  <div
+    class="tooltip"
+    :class="{ 'is-visible': visible }"
+    :style="{ left: `${x}px`, top: `${y}px` }"
+    v-html="html"
+  />
+</template>
